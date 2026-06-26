4d89b1aca361eca176d5db705363c7c0aebe3573
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-26 10:59:57 -0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,19 +5,17 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-android {    
-        // ndkVersion = flutter.ndkVersion
-    // ndkVersion = "27.0.12077973"
-    ndkVersion = "26.3.11579264"
-    namespace = "com.skoon.muslim.app"
-    compileSdk = flutter.compileSdkVersion
+android {
+    // Use uma versão do NDK compatível ou remova a linha para usar a padrão
+    ndkVersion = "27.0.12077973"  // ou remova se não for essencial
 
+    namespace = "com.skoon.muslim.app"
+    compileSdk = 35  // <-- DEFINIDO EXPLICITAMENTE
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-                isCoreLibraryDesugaringEnabled = true // <-- Add this line
-
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -25,20 +23,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.skoon.muslim.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35  // <-- DEFINIDO EXPLICITAMENTE
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -47,7 +40,7 @@ android {
 flutter {
     source = "../.."
 }
+
 dependencies {
-    // ...other dependencies...
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
