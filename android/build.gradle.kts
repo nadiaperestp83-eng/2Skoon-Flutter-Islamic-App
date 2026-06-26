f8251b9b7460fafca3d2f1f495a5118e3e7ad8a0
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-26 11:04:32 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,17 @@
+buildscript {
+    ext.kotlin_version = '1.9.22'  // Versão estável do Kotlin
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        // Gradle plugin (versão compatível com o Flutter)
+        classpath 'com.android.tools.build:gradle:7.4.2'  // ou 8.1.0, mas 7.4.2 é estável
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +19,7 @@ allprojects {
     }
 }
 
+// Configuração do diretório de build (opcional, mas mantida)
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -12,33 +27,22 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-subprojects {
-    afterEvaluate {
-        if (hasProperty("android")) {
-            configure<com.android.build.gradle.BaseExtension> {
-                if (namespace == null) {
-                    namespace = project.group.toString()  // Set namespace as fallback
-                }
-                
-                tasks.whenTaskAdded {
-                    if (name.contains("processDebugManifest") || name.contains("processReleaseManifest")) {
-                        doFirst {
-                            val manifestFile = file("${projectDir}/src/main/AndroidManifest.xml")
-                            if (manifestFile.exists()) {
-                                var manifestContent = manifestFile.readText()
-                                if (manifestContent.contains("package=")) {
-                                    manifestContent = manifestContent.replace(Regex("""package="[^"]*""""), "")
-                                    manifestFile.writeText(manifestContent)
-                                    println("Removed 'package' attribute from $manifestFile")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+
+// Remova a lógica abaixo se não for necessária (pode causar problemas)
+// subprojects {
+//     afterEvaluate {
+//         if (hasProperty("android")) {
+//             configure<com.android.build.gradle.BaseExtension> {
+//                 if (namespace == null) {
+//                     namespace = project.group.toString()
+//                 }
+//                 // ... lógica para remover package do manifest
+//             }
+//         }
+//     }
+// }
+
+// Se precisar forçar a avaliação do app
 subprojects {
     project.evaluationDependsOn(":app")
 }
