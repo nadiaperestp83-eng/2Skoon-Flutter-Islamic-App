3d00085760497b9bf9770d422241ca7cae23d78e
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-26 11:15:29 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,12 @@
 buildscript {
-    ext.kotlin_version = '1.9.22'  // Versão estável do Kotlin
+    extra["kotlin_version"] = "1.9.22"
     repositories {
         google()
         mavenCentral()
     }
-
     dependencies {
-        // Gradle plugin (versão compatível com o Flutter)
-        classpath 'com.android.tools.build:gradle:7.4.2'  // ou 8.1.0, mas 7.4.2 é estável
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
     }
 }
 
@@ -19,30 +17,14 @@ allprojects {
     }
 }
 
-// Configuração do diretório de build (opcional, mas mantida)
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
 
-// Remova a lógica abaixo se não for necessária (pode causar problemas)
-// subprojects {
-//     afterEvaluate {
-//         if (hasProperty("android")) {
-//             configure<com.android.build.gradle.BaseExtension> {
-//                 if (namespace == null) {
-//                     namespace = project.group.toString()
-//                 }
-//                 // ... lógica para remover package do manifest
-//             }
-//         }
-//     }
-// }
-
-// Se precisar forçar a avaliação do app
 subprojects {
     project.evaluationDependsOn(":app")
 }
